9de11e1cd4253aa567fccd3670eda2e301c615c6
Hossein Nojavan <hosein.nojawan@gmail.com>
2026-08-08 16:26:08 +0430

    CRUD of projects added

--- proj_mngmnt_system/projects/urls.py
+++ proj_mngmnt_system/projects/urls.py
@@ -6,7 +6,10 @@ app_name = 'projects'
 
 urlpatterns = [
     path('',ProjectListView.as_view(), name='projects_list'),
-    path('add-project',AddProjectView.as_view(), name='projects_add')
+    path('projects_add',AddProjectView.as_view(), name='projects_add'),
+    path('projects_detail/<int:proj_id>',ProjectDetailView.as_view(), name='projects_detail'),
+    path('delete/<int:proj_id>',ProjectDeleteView.as_view(), name='projects_delete'),
+    path('update/<int:proj_id>',ProjectUpdateView.as_view(), name='projects_update'),
     
     
 ]
--- proj_mngmnt_system/projects/views.py
+++ proj_mngmnt_system/projects/views.py
@@ -1,28 +1,170 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
+
 from projects.forms import ProjectForm
 from projects.models import Project
-from django.contrib import messages
 
-class ProjectListView(View):
+
+class ProjectListView(LoginRequiredMixin, View):
+
     def get(self, request):
-        projs = Project.objects.all()
-        return render(request, 'projects/project_list.html', {'projs':projs})
-    
-    
-class AddProjectView(View):
+        projects = Project.objects.filter(
+            Q(owner=request.user) |
+            Q(members=request.user)
+        ).distinct()
+
+        return render(
+            request,
+            "projects/project_list.html",
+            {"projs": projects}
+        )
+
+
+class AddProjectView(LoginRequiredMixin, View):
     form_class = ProjectForm
+
     def get(self, request):
-        
-        return render(request, 'projects/project_add.html', {'form':self.form_class})   
-    
+        form = self.form_class()
+
+        return render(
+            request,
+            "projects/project_add.html",
+            {"form": form}
+        )
+
     def post(self, request):
         form = self.form_class(request.POST)
-        
+
         if form.is_valid():
-            new_project = form.save(commit=False)
-            new_project.owner = request.user
-            new_project.save()
-            messages.success(request, 'your project added successfully...! ', 'success')
-            return redirect('projects:projects_list')
-        pass
+            project = form.save(commit=False)
+
+            project.owner = request.user
+            project.save()
+
+            # ذخیره ManyToMany یعنی members
+            form.save_m2m()
+
+            messages.success(
+                request,
+                "Your project was added successfully."
+            )
+
+            return redirect("projects:projects_list")
+
+        return render(
+            request,
+            "projects/project_add.html",
+            {"form": form}
+        )
+
+
+class ProjectDetailView(LoginRequiredMixin, View):
+
+    def get(self, request, proj_id):
+
+        projects = Project.objects.filter(
+            Q(owner=request.user) |
+            Q(members=request.user)
+        ).distinct()
+
+        project = get_object_or_404(
+            projects,
+            id=proj_id
+        )
+
+        return render(
+            request,
+            "projects/project_detail.html",
+            {"project": project}
+        )
+
+
+class ProjectUpdateView(LoginRequiredMixin, View):
+    form_class = ProjectForm
+
+    def get_project(self, request, proj_id):
+        return get_object_or_404(
+            Project,
+            id=proj_id,
+            owner=request.user
+        )
+
+    def get(self, request, proj_id):
+        project = self.get_project(request, proj_id)
+
+        form = self.form_class(
+            instance=project
+        )
+
+        return render(
+            request,
+            "projects/project_edit.html",
+            {
+                "project": project,
+                "form": form,
+            }
+        )
+
+    def post(self, request, proj_id):
+        project = self.get_project(request, proj_id)
+
+        form = self.form_class(
+            request.POST,
+            instance=project
+        )
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(
+                request,
+                "Project edited successfully."
+            )
+
+            return redirect(
+                "projects:projects_detail",
+                proj_id=project.id
+            )
+
+        return render(
+            request,
+            "projects/project_edit.html",
+            {
+                "project": project,
+                "form": form,
+            }
+        )
+
+
+class ProjectDeleteView(LoginRequiredMixin, View):
+
+    def get_project(self, request, proj_id):
+        return get_object_or_404(
+            Project,
+            id=proj_id,
+            owner=request.user
+        )
+
+    def get(self, request, proj_id):
+        project = self.get_project(request, proj_id)
+
+        return render(
+            request,
+            "projects/project_delete.html",
+            {"project": project}
+        )
+
+    def post(self, request, proj_id):
+        project = self.get_project(request, proj_id)
+
+        project.delete()
+
+        messages.success(
+            request,
+            "Project deleted successfully."
+        )
+
+        return redirect("projects:projects_list")
